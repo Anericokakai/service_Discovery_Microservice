560d44ab4af33b97aa98e20e113b5ce519ba47be
Anericokakai <challykiddoh@gmail.com>
2023-10-15 21:27:36 +0300

    discovery service

--- src/main/java/com/discoveryService/DiscoveryService_Ecommerce/DiscoveryServiceEcommerceApplication.java
+++ src/main/java/com/discoveryService/DiscoveryService_Ecommerce/DiscoveryServiceEcommerceApplication.java
@@ -2,8 +2,10 @@ package com.discoveryService.DiscoveryService_Ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceEcommerceApplication {
 
 	public static void main(String[] args) {
