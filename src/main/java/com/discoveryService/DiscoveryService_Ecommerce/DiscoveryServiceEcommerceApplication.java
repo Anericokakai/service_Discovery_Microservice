410dc1c06c40d2fe405401b53a924b814201c389
Anericokakai <challykiddoh@gmail.com>
2023-10-15 12:41:02 +0300

    service dicovery for client side

---
+++ src/main/java/com/discoveryService/DiscoveryService_Ecommerce/DiscoveryServiceEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.discoveryService.DiscoveryService_Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceEcommerceApplication.class, args);
+	}
+
+}
